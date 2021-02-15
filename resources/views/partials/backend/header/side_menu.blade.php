afe5fdff7a8f108131c60a887d8e49f5207cab61
ahmed1322 <=>
2021-02-15 17:56:50 +0200

    Added Restrictions to Dashbnoard Side manu Links

--- resources/views/partials/backend/header/side_menu.blade.php
+++ resources/views/partials/backend/header/side_menu.blade.php
@@ -4,7 +4,7 @@
         <img src="{{ Gravatar::src(auth()->user()->email) }}" class="avatar-sm rounded-circle mr-2" alt="Shreyu" />
         <img src="{{ Gravatar::src(auth()->user()->email) }}" class="avatar-xs rounded-circle mr-2" alt="Shreyu" />
         @php
-            // dd(auth()->user()->role_id)
+            // dump(auth()->user()->posts())
         @endphp
         <div class="media-body">
             <h6 class="pro-user-name mt-0 mb-0">
@@ -51,6 +51,29 @@
         <!--- Sidemenu -->
         <div id="sidebar-menu" class="slimscroll-menu">
             <ul class="metismenu" id="menu-bar">
+
+                @if ( auth()->user()->is_admin() )
+                    <li>
+                        <a href="{{ route('settings.index') }}">
+                            <i data-feather="settings"></i>
+                            <span> Settings </span>
+                        </a>
+                    </li>
+                @endif
+
+                <li>
+                    <a href="{{ route('profile.index') }}">
+                        <i data-feather="image"></i>
+                        <span> Profile </span>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="{{ route('dashboard') }}">
+                        <i data-feather="pie-chart"></i>
+                        <span> Dashboard </span>
+                    </a>
+                </li>
 
                 @if ( auth()->user()->is_admin() )
 
@@ -195,79 +218,8 @@
 
                 @endif
 
-                <li>
-                    <a href="{{ route('profile.index') }}">
-                        <i data-feather="image"></i>
-                        <span> Profile </span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="{{ route('dashboard') }}">
-                        <i data-feather="pie-chart"></i>
-                        <span> Dashboard </span>
-                        {{-- <span class="menu-arrow"></span> --}}
-                    </a>
-                </li>
-
-                @if (! auth()->user()->status && ! empty(auth()->user()->category_roles()))
-                    <li>
-                        <a href="javascript: void(0);">
-                            <i class="uil uil-briefcase"></i>
-                            <span> Category </span>
-                            <span class="menu-arrow"></span>
-                        </a>
-
-                        <ul class="nav-second-level" aria-expanded="false">
-                            @isset(auth()->user()->category_roles()['r'])
-                                <li>
-                                    <a href="{{ route('category.index') }}">
-                                        <i class="uil uil-suitcase-alt"></i>
-                                        <span class="ml-2">All Categories</span>
-                                    </a>
-                                </li>
-                            @endisset
-
-                            @isset(auth()->user()->category_roles()['c'])
-                                <li>
-                                    <a href="{{ route('category.create') }}">
-                                        <i class="uil uil-wall"></i>
-                                        <span class="ml-2">Add new Category</span>
-                                    </a>
-                                </li>
-                            @endisset
-
-                        </ul>
-                    </li>
-                @elseif(auth()->user()->status)
-
-                    <li>
-                        <a href="javascript: void(0);">
-                            <i class="uil uil-briefcase"></i>
-                            <span> Category </span>
-                            <span class="menu-arrow"></span>
-                        </a>
-
-                        <ul class="nav-second-level" aria-expanded="false">
-                            <li>
-                                <a href="{{ route('category.index') }}">
-                                    <i class="uil uil-suitcase-alt"></i>
-                                    <span class="ml-2">All Categories</span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="{{ route('category.create') }}">
-                                    <i class="uil uil-wall"></i>
-                                    <span class="ml-2">Add new Category</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                @endif
-
-                @if (! empty( auth()->user()->post_roles() ) )
+                {{-- Posts --}}
+                @if ( auth()->user()->has_post_roles() ) 
                 <li>
                     <a href="javascript: void(0);">
                         <i class="uil uil-comment-edit"></i>
@@ -276,9 +228,9 @@
                     </a>
 
                     <ul class="nav-second-level" aria-expanded="false">
-                        @if (auth()->user()->can_view_posts())
+                        @if ( auth()->user()->can_view_posts() )
                         <li>
-                            <a href="{{ route('posts.index') }}">
+                            <a href="{{ route('post.index') }}">
                                 <i class="uil uil-suitcase-alt"></i>
                                 <span class="ml-2">All Posts</span>
                             </a>
@@ -297,16 +249,41 @@
                 </li>
                 @endif
 
+                {{-- Categories --}}
+                @if ( auth()->user()->has_category_roles() )
+                    <li>
+                        <a href="javascript: void(0);">
+                            <i class="uil uil-briefcase"></i>
+                            <span> Category </span>
+                            <span class="menu-arrow"></span>
+                        </a>
 
-                <li>
-                    <a href="javascript:void(0);">
-                        <i class="uil uil-comment-alt-message"></i>
-                        <span> Comment </span>
-                    </a>
-                </li>
+                        <ul class="nav-second-level" aria-expanded="false">
 
-                @if ( auth()->user()->is_admin() )
+                            @if( auth()->user()->can_view_categories() )
+                                <li>
+                                    <a href="{{ route('category.index') }}">
+                                        <i class="uil uil-suitcase-alt"></i>
+                                        <span class="ml-2">All Categories</span>
+                                    </a>
+                                </li>
+                            @endif
 
+                            @if(auth()->user()->can_create_category())
+                                <li>
+                                    <a href="{{ route('category.create') }}">
+                                        <i class="uil uil-wall"></i>
+                                        <span class="ml-2">Add new Category</span>
+                                    </a>
+                                </li>
+                            @endif
+
+                        </ul>
+                    </li>
+                @endif
+
+                {{-- Tags --}}
+                @if ( auth()->user()->has_tag_roles() )
                     <li>
                         <a href="javascript: void(0);">
                             <i class="uil uil-expand-arrows-alt"></i>
@@ -315,85 +292,98 @@
                         </a>
 
                         <ul class="nav-second-level" aria-expanded="false">
+                            @if( auth()->user()->can_view_tags() )
                             <li>
                                 <a href="{{ route('tag.index') }}">
                                     <i class="uil uil-suitcase-alt"></i>
                                     <span class="ml-2">All Tags</span>
                                 </a>
                             </li>
+                            @endif
 
+                            @if( auth()->user()->can_create_tag() )
                             <li>
                                 <a href="{{ route('tag.create') }}">
                                     <i class="uil uil-wall"></i>
                                     <span class="ml-2">Add new Tag</span>
                                 </a>
                             </li>
+                            @endif
                         </ul>
                     </li>
-
-                    <li>
-                        <a href="javascript: void(0);">
-                            <i class="uil uil-users-alt"></i>
-                            <span> User </span>
-                            <span class="menu-arrow"></span>
-                        </a>
-
-                        <ul class="nav-second-level" aria-expanded="false">
-                            <li>
-                                <a href="{{ route('user.index') }}">
-                                    <i class="uil uil-suitcase-alt"></i>
-                                    <span class="ml-2">All Users</span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="{{ route('user.admins') }}">
-                                    <i class="uil uil-user-plus"></i>
-                                    <span>Admin only</span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="{{ route('user.create') }}">
-                                    <i class="uil uil-wall"></i>
-                                    <span class="ml-2">Add new User</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li>
-                        <a href="javascript:void(0);">
-                            <i class="uil uil-receipt-alt"></i>
-                            <span> Role </span>
-                            <span class="menu-arrow"></span>
-                        </a>
-
-                        <ul class="nav-second-level" aria-expanded="false">
-                            <li>
-                                <a href="{{ route('role.index') }}">
-                                    <i class="uil uil-suitcase-alt"></i>
-                                    <span class="ml-2">All Roles</span>
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="{{ route('role.create') }}">
-                                    <i class="uil uil-wall"></i>
-                                    <span class="ml-2">Add new Role</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-
-                    <li>
-                        <a href="javascript:void(0);">
-                            <i class="uil uil-envelope-send"></i>
-                            <span> Send Msg </span>
-                        </a>
-                    </li>
-
                 @endif
+
+                @if ( auth()->user()->is_admin() )
+                <li>
+                    <a href="javascript: void(0);">
+                        <i class="uil uil-users-alt"></i>
+                        <span> User </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="{{ route('user.index') }}">
+                                <i class="uil uil-suitcase-alt"></i>
+                                <span class="ml-2">All Users</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('user.admins') }}">
+                                <i class="uil uil-user-plus"></i>
+                                <span>Admin only</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('user.create') }}">
+                                <i class="uil uil-wall"></i>
+                                <span class="ml-2">Add new User</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li>
+                    <a href="javascript:void(0);">
+                        <i class="uil uil-receipt-alt"></i>
+                        <span> Role </span>
+                        <span class="menu-arrow"></span>
+                    </a>
+
+                    <ul class="nav-second-level" aria-expanded="false">
+                        <li>
+                            <a href="{{ route('role.index') }}">
+                                <i class="uil uil-suitcase-alt"></i>
+                                <span class="ml-2">All Roles</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="{{ route('role.create') }}">
+                                <i class="uil uil-wall"></i>
+                                <span class="ml-2">Add new Role</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                
+                <li>
+                    <a href="javascript:void(0);">
+                        <i class="uil uil-envelope-send"></i>
+                        <span> Send Msg </span>
+                    </a>
+                </li>
+                @endif
+
+                <li>
+                    <a href="javascript:void(0);">
+                        <i class="uil uil-comment-alt-message"></i>
+                        <span> Comment </span>
+                    </a>
+                </li>
+
                 @if (Route::has('logout'))
                 <li>
                     <a href="{{ route('logout') }}"
